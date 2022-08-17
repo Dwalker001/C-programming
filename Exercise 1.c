986fbec5ca91a703618bc62a0e67428ea562ed68
Dwalker001 <clintonakpan2019@gmail.com>
2022-08-17 09:20:36 -0700

    C Higher basics

---
+++ Exercise 1.c
@@ -0,0 +1,8 @@
+
+main() /* Main begins the execution */
+{
+
+    printf("My name is Clinton \n"); /*Prints my name*/
+    printf("I base @ Minna, Niger state, Shiroro.\n"); /*Prints out the address in code line*/
+    printf("Please reach out to me @ 08157442663 \n"); /*Prints out the contact/phone_number details*/
+}
---
+++ Exercise 2.c
@@ -0,0 +1,59 @@
+/*******************************************************************************************************/
+ /* a program that writes a name 10 times, */
+ /* each with a different loop style */
+ /* a program that counts from one to ten, */
+ /* prints the values on a separate line for each, */
+ /* and includes a message of your choice when the count is 3 and a different message when the */
+ /* count is 7. */
+ /******************************************************************************************************/
+main()
+{
+    int n;
+    char Clinton;
+    n = "Clinton";
+    for (Clinton = 0; Clinton < 10; Clinton++)
+    printf("%s\n",n);
+    printf("Done!\n\n");
+/* End of the for loop */
+
+    int m;
+    char Philip;
+    m = "Philip";
+        do{
+            printf("%s\n",m); // The do..while loop
+            Philip++;
+        }
+        while(Philip < 10);
+printf("Done!\n\n");
+// End of the do...while loop
+
+    int o;
+    char Akpan;
+    o = "Akpan";
+        printf("%s\n",o);
+        Akpan++;
+        while(Akpan < 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
